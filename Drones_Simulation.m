2c5f28631922e041869d44edf1c43354ba334f44
Mitchell-Griggs <mitchelltgriggs@gmail.com>
2026-01-04 03:17:34 +1000

    Update Drones_Simulation.m

--- Drones_Simulation.m
+++ Drones_Simulation.m
@@ -16,81 +16,87 @@ X0(:,1,1) = [1;1]/3;
 X0(:,2,1) = [0;1]/3;
 X0(:,3,1) = [1;0]/5;
 X = zeros(2,nd,N);
-Y0 = [0;0];
-Y = zeros(2,1,N);
+Z0 = [0;0];
+Z = zeros(2,1,N);
 X3tracker = ones(2,N);
-Ytracker = ones(2,N);
-Yd3tracker = ones(2,N);
+Ztracker = ones(2,N);
+Zd3tracker = ones(2,N);
 for j = 1:N
     if j == 1
-        Y(:,1,j) = Y0+beta*X0(:,1,:)*dt;
-        Yd3tracker(:,j) = Y0;
+        Z(:,1,j) = Z0+beta*X0(:,1,:)*dt;
+        Zd3tracker(:,j) = Z0;
     elseif j <= 20
-        Y(:,1,j) = Y(:,1,j-1) + beta*X0(:,3,:)*dt;
-        Yd3tracker(:,j) = Y0;
+        Z(:,1,j) = Z(:,1,j-1) + beta*X0(:,3,:)*dt;
+        Zd3tracker(:,j) = Z0;
     else
-        if t(j) <= 8.5
-            Y(:,1,j) = Y(:,1,j-1) + beta*(X(:,3,20*floor((j-1)/20))-Y(:,1,j-1))*dt;
+        if t(j) <= 8.5 + 1.5
+            Z(:,1,j) = Z(:,1,j-1) + beta*(X(:,3,20*floor((j-1)/20))-Z(:,1,j-1))*dt;
         else
-            Y(:,1,j) = Y(:,1,j-1);
+            Z(:,1,j) = Z(:,1,j-1);
         end
-        Yd3tracker(:,j) = Y(:,1,j-20);
+        Zd3tracker(:,j) = Z(:,1,j-20);
     end
     for p = 1:nd
         if j == 1
-            Yd = Y0;
-            X(:,p,j) = X0(:,p,1) + 0.5*v(0,tau(p),X0(:,p,1),Y0,Yd)/norm(v(0,tau(p),X0(:,p,1),Y0,Yd))*dt + sigmaj(0,tau(p),X0(:,p,1),Y0,Yd)*0;
+            Zd = Z0;
+            X(:,p,j) = X0(:,p,1) + 0.5*v(0,tau(p),X0(:,p,1),Z0,Zd)/norm(v(0,tau(p),X0(:,p,1),Z0,Zd))*dt + sigmaj(0,tau(p),X0(:,p,1),Z0,Zd)*0;
         else    
             if j <= 21
-                Yd = Y0;
+                Zd = Z0;
             else
-                Yd = Y(:,1,j-21);
+                Zd = Z(:,1,j-21);
             end
-            X(:,p,j) = X(:,p,j-1) + 0.5*v(t(j-1),tau(p),X(:,p,j-1),Y(:,1,j-1),Yd)/norm(v(t(j-1),tau(p),X(:,p,j-1),Y(:,1,j-1),Yd))*dt + sigmaj(t(j-1),tau(p),X(:,p,j-1),Y(:,1,j-1),Yd)*(W(:,p,j)-W(:,p,j-1));
+            X(:,p,j) = X(:,p,j-1) + 0.5*v(t(j-1),tau(p),X(:,p,j-1),Z(:,1,j-1),Zd)/norm(v(t(j-1),tau(p),X(:,p,j-1),Z(:,1,j-1),Zd))*dt + sigmaj(t(j-1),tau(p),X(:,p,j-1),Z(:,1,j-1),Zd)*(W(:,p,j)-W(:,p,j-1));
         end
     end
     X3tracker(:,j) = X(:,3,j);
-    Ytracker(:,j) = Y(:,1,j);
+    Ztracker(:,j) = Z(:,1,j);
     % The plot:
+    %pause(1)  % Use this to test and see each frame slowly.
     figure(1)
     clf;hold on
-    L1=plot(nan,nan,'*','Color',[0,0,1],'linewidth',10);LX3=plot(nan,nan,'b');
-    L2=plot(nan,nan,'*','Color',[1,0,0],'linewidth',10);LY=plot(nan,nan,'r');
+    LegendX1=plot(nan,nan,'*','Color',[0,1,1]*2/3,'linewidth',10);
+    LegendX2=plot(nan,nan,'*','Color',[1,0,1]*2/3,'linewidth',10);
+    LegendX3=plot(nan,nan,'*','Color',[0,0,1],'linewidth',10);LX3=plot(nan,nan,'b');
+    L2=plot(nan,nan,'*','Color',[1,0,0],'linewidth',10);LZ=plot(nan,nan,'r');
     L3=plot(nan,nan,'*','Color',[0,2/3,0],'linewidth',10);L4=plot(nan,nan,'--','Color',[0,2/3,0],'linewidth',1);
     L5=plot(nan,nan,'k:','LineWidth',1);
-    legend([L1,LX3,L2,LY,L3,L4,L5],{'$X_j(t)$, $j=1,2,3$','$Y(t)$','$Y(t-1)$','$X_3(t)$ Path','$Y(t)$ Path','$Y(t-1)$ Field of View','Jittering Axis'},'Interpreter','latex','Location','northwest','NumColumns',1,'FontSize',16);
+    legend([LegendX1,LegendX2,LegendX3,LX3,L2,LZ,L3,L4,L5],{'$X_1(t)$','$X_2(t)$','$X_3(t)$','path of $X_3(t)$','$Z(t)$','path of $Z(t)$','$Z(t-1)$','field of view from $Z(t-1)$','jittering axis'},'Interpreter','latex','Location','northwest','NumColumns',1,'FontSize',16);
     
     if j <= 20
-        Yd = Y0;
+        Zd = Z0;
     else
-        Yd = Y(:,1,j-20);
+        Zd = Z(:,1,j-20);
     end
-    direction = X(:,3,j) - Yd;
+    direction = X(:,3,j) - Zd;
     
     range = norm(direction);
     theta = atan2(direction(2),direction(1))-pi/4:0.01:atan2(direction(2),direction(1))+pi/4;
-    y1circle = Yd(1) + range*cos(theta); y2circle = Yd(2) + range*sin(theta);
+    y1circle = Zd(1) + range*cos(theta); y2circle = Zd(2) + range*sin(theta);
     plot(y1circle,y2circle,'--','Color',[0,2/3,0],'linewidth',1,'HandleVisibility','off')
     perp = [-direction(2); direction(1)] / norm(direction);
     Point1 = X(:,3,j) - 2*perp;
     Point2 = X(:,3,j) + 2*perp;
     plot([Point1(1) Point2(1)], [Point1(2) Point2(2)], 'k:', 'LineWidth', 1, 'HandleVisibility','off');
     plot(X3tracker(1,1:j),X3tracker(2,1:j),'b','LineWidth',1,'HandleVisibility','off')
-    plot(Ytracker(1,1:j),Ytracker(2,1:j),'r','LineWidth',1,'HandleVisibility','off')
+    plot(Ztracker(1,1:j),Ztracker(2,1:j),'r','LineWidth',1,'HandleVisibility','off')
 
-    for p = 1:nd
-        plot(X(1,p,j),X(2,p,j),'*','Color',[0,0,1],'linewidth',10,'HandleVisibility','off')
-    end
-    plot(Y(1,1,j),Y(2,1,j),'*','Color',[1,0,0],'linewidth',10,'HandleVisibility','off')
+    % for p = 1:nd
+    %     plot(X(1,p,j),X(2,p,j),'*','Color',[0,0,1],'linewidth',10,'HandleVisibility','off')
+    % end
+    plot(X(1,1,j),X(2,1,j),'*','Color',[0,1,1]*2/3,'linewidth',10,'HandleVisibility','off')
+    plot(X(1,2,j),X(2,2,j),'*','Color',[1,0,1]*2/3,'linewidth',10,'HandleVisibility','off')
+    plot(X(1,3,j),X(2,3,j),'*','Color',[0,0,1],'linewidth',10,'HandleVisibility','off')
+    plot(Z(1,1,j),Z(2,1,j),'*','Color',[1,0,0],'linewidth',10,'HandleVisibility','off')
     if j <= 20
-        plot(Y(1,1,1),Y(2,1,1),'*','Color',[0,2/3,0],'linewidth',10,'HandleVisibility','off')
+        plot(Z(1,1,1),Z(2,1,1),'*','Color',[0,2/3,0],'linewidth',10,'HandleVisibility','off')
     else
-        plot(Y(1,1,j-20),Y(2,1,j-20),'*','Color',[0,2/3,0],'linewidth',10,'HandleVisibility','off')
+        plot(Z(1,1,j-20),Z(2,1,j-20),'*','Color',[0,2/3,0],'linewidth',10,'HandleVisibility','off')
     end
 
     set(gca, 'XTick', [], 'YTick', [])
     set(gcf,'position',[200,200,800,800])
-    axis([-8,9,-4,13])
+    axis([-4,7,-2,9])
     xlabel('')
     ylabel('')
     hold off
@@ -100,41 +106,41 @@ for j = 1:N
     if savefigures == 1
         if t(j) == 0.2
             title('')
-            saveas(gcf,'DronesFigure1.eps','epsc')
+            saveas(gcf,'ZExample5A.eps','epsc')
         elseif t(j) == 0.5
             legend off
             title('')
-            saveas(gcf,'DronesFigure2.eps','epsc')
+            saveas(gcf,'ZExample5B.eps','epsc')
         elseif t(j) == 1
-            legend off
+            %legend off
             title('')
-            saveas(gcf,'DronesFigure3.eps','epsc')
+            saveas(gcf,'ZExample5C.eps','epsc')
         elseif t(j) == 8
             legend off
             title('')
-            saveas(gcf,'DronesFigure4.eps','epsc')
+            saveas(gcf,'ZExample5D.eps','epsc')
         elseif t(j) == 9
             legend off
             title('')
-            saveas(gcf,'DronesFigure5.eps','epsc')
+            saveas(gcf,'ZExample5E.eps','epsc')
         elseif t(j) == 10
             legend off
             title('')
-            saveas(gcf,'DronesFigure6.eps','epsc')
+            saveas(gcf,'ZExample5F.eps','epsc')
         end
     end
 end
 
 
-function Vvalue = v(t,dj,X,Y,Yd)
+function Vvalue = v(t,dj,X,Z,Zd)
     if t < dj
-        Vvalue = X-Y;
+        Vvalue = X-Z;
     else
-        Vvalue = X-Yd;
+        Vvalue = X-Zd;
     end
 end
 
-function sigmajvalue = sigmaj(t,dj,X,Y,Yd)
-    V = v(t,dj,X,Y,Yd);
+function sigmajvalue = sigmaj(t,dj,X,Z,Zd)
+    V = v(t,dj,X,Z,Zd);
     sigmajvalue = norm(V)^(-1)*[-V(2);V(1)];
 end
